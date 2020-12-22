ed40c9fd809e80c4ca162b80343bbe3c35b75a50
Mindaugas Urbontaitis <m@urbontaitis.lt>
2020-12-22 15:15:33 +0200

    end of refactoring. Need to fix fullHouse implementation

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -4,12 +4,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 class DiceHand implements Iterable<Integer> {
 
@@ -17,6 +19,9 @@ class DiceHand implements Iterable<Integer> {
 
   DiceHand(int d1, int d2, int d3, int d4, int d5) {
     this.dice = new int[] {d1, d2, d3, d4, d5};
+    if (stream().anyMatch(d -> d <= 0 || d >= 7)) {
+      throw new IllegalArgumentException();
+    }
   }
 
   public Map<Integer, Long> getCountMap() {
@@ -149,60 +154,59 @@ public class Yatzy {
     return 0;
   }
 
-  public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-    int[] tallies;
-    tallies = new int[6];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1)
+  public static int smallStraight(DiceHand diceHand) {
+    var sorted = diceHand.stream().collect(toSet());
+    if(Set.of(1, 2, 3, 4, 5).equals(sorted)) {
       return 15;
+    }
     return 0;
   }
 
-  public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-    int[] tallies;
-    tallies = new int[6];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1)
+  public static int largeStraight(DiceHand diceHand) {
+    var sorted = diceHand.stream().collect(toSet());
+    if(Set.of(2, 3, 4, 5, 6).equals(sorted)) {
       return 20;
+    }
     return 0;
   }
 
-  public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
-    int[] tallies;
-    boolean _2 = false;
-    int i;
-    int _2_at = 0;
-    boolean _3 = false;
-    int _3_at = 0;
+  public static int fullHouse(DiceHand diceHand) {
+    int scorePair = score_pair(diceHand);
+    int threeOfAKind = three_of_a_kind(diceHand);
 
-    tallies = new int[6];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
+    // FIXME scorePair return not pair of (1, 1), but (2,2), which is three of kind (2, 2, 2)
+    if ( scorePair > 0 && threeOfAKind > 0 ) {
+       return scorePair + threeOfAKind;
+    }
+    return 0;
 
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 2) {
-        _2 = true;
-        _2_at = i + 1;
-      }
-
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 3) {
-        _3 = true;
-        _3_at = i + 1;
-      }
-
-    if (_2 && _3) return _2_at * 2 + _3_at * 3;
-    else return 0;
+//    int[] three_of_a_kindllies;
+//    boolean _2 = false;
+//    int i;
+//    int _2_at = 0;
+//    boolean _3 = false;
+//    int _3_at = 0;
+//
+//    int[] tallies = new int[6];
+//    tallies[diceHand.die(0) - 1] += 1;
+//    tallies[diceHand.die(1) - 1] += 1;
+//    tallies[diceHand.die(2) - 1] += 1;
+//    tallies[diceHand.die(3) - 1] += 1;
+//    tallies[diceHand.die(4) - 1] += 1;
+//
+//    for (i = 0; i < 6; i += 1)
+//      if (tallies[i] == 2) {
+//        _2 = true;
+//        _2_at = i + 1;
+//      }
+//
+//    for (i = 0; i < 6; i += 1)
+//      if (tallies[i] == 3) {
+//        _3 = true;
+//        _3_at = i + 1;
+//      }
+//
+//    if (_2 && _3) return _2_at * 2 + _3_at * 3;
+//    else return 0;
   }
 }
--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -3,6 +3,16 @@ import static org.junit.Assert.*;
 
 public class YatzyTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThroughExceptionWhenNegativeValueIsGiven() {
+        new DiceHand(-1, 1, 1, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThroughExceptionWhenDiceValueIsMoreThanSix() {
+        new DiceHand(1, 7, 1, 1, 1);
+    }
+
     @Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
@@ -93,21 +103,24 @@ public class YatzyTest {
 
     @Test
     public void smallStraight() {
-        assertEquals(15, Yatzy.smallStraight(1,2,3,4,5));
-        assertEquals(15, Yatzy.smallStraight(2,3,4,5,1));
-        assertEquals(0, Yatzy.smallStraight(1,2,2,4,5));
+        assertEquals(15, Yatzy.smallStraight(new DiceHand(1, 2, 3, 4, 5)));
+        assertEquals(15, Yatzy.smallStraight(new DiceHand(2, 3, 4, 5, 1)));
+        assertEquals(0, Yatzy.smallStraight(new DiceHand(1, 2, 2, 4, 5)));
     }
 
     @Test
     public void largeStraight() {
-        assertEquals(20, Yatzy.largeStraight(6,2,3,4,5));
-        assertEquals(20, Yatzy.largeStraight(2,3,4,5,6));
-        assertEquals(0, Yatzy.largeStraight(1,2,2,4,5));
+        assertEquals(20, Yatzy.largeStraight(new DiceHand(6, 2, 3, 4, 5)));
+        assertEquals(20, Yatzy.largeStraight(new DiceHand(2, 3, 4, 5, 6)));
+        assertEquals(0, Yatzy.largeStraight(new DiceHand(1, 2, 2, 4, 5)));
     }
 
     @Test
     public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+        assertEquals(18, Yatzy.fullHouse(new DiceHand(6, 2, 2, 2, 6)));
+        assertEquals(8, Yatzy.fullHouse(new DiceHand(1, 1, 2, 2, 2)));
+        assertEquals(0, Yatzy.fullHouse(new DiceHand(2, 3, 4, 5, 6)));
+        assertEquals(0, Yatzy.fullHouse(new DiceHand(2, 2, 3, 3, 4)));
+        assertEquals(0, Yatzy.fullHouse(new DiceHand(4, 4, 4, 4, 4)));
     }
 }
